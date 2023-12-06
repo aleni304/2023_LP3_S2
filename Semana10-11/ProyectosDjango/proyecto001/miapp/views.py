581f4aaaa6d23452530c6dafc370b71f764d103c
Alexia Asunción <alexiaap_304@hotmail.com>
2023-12-06 11:15:35 -0500

    Lenguaje de plantillas

--- Semana10-11/ProyectosDjango/proyecto001/miapp/views.py
+++ Semana10-11/ProyectosDjango/proyecto001/miapp/views.py
@@ -22,24 +22,38 @@ layout = """
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    estudiantes = ['Alexia Asunción',
+                    'Jheremy Ayquipa',
+                    'Axel Barzola',
+                    'Josue Caman',
+                    'Carlos Curo',
+                    'Brian Inca',
+                    'Gerson Sahuma',
+                    'Jhon Urbisagastegui',
+                    'Elias Talledo'
+                    ]
+    return render(request,'index.html',{
+        'titulo':'Inicio',
+        'mensaje':'Proyecto web con DJango',
+        'estudiantes':estudiantes
+    })
 
 def saludo(request):
-    return render(request,'saludo.html')
+    return render(request,'saludo.html',{
+        'titulo':'Saludo',
+        'autor':'Alexia Asunción'
+    })
 
 def rango(request):
     a = 10
     b = 20
-    resultado = f"""
-                <h2> Números de [{a},{b}]</h2>
-                Resultado: <br>
-                <ul>
-    """
-    while a <= b:
-        resultado += f"<li> {a} </li>"
-        a += 1
-    resultado += "</ul>"
-    return HttpResponse(layout+resultado)
+    rango_numeros = range(a,b+1)
+    return render(request,'rango.html',{
+        'titulo':'Rango',
+        'a':a,
+        'b':b,
+        'rango_numeros':rango_numeros
+    })
 
 def rango2(request,a,b):
     if a > b:
